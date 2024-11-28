d93c829c1640ceaec40763b689c7ed7008a2f04a
Luis Laguardia <61608109+luislaguardia@users.noreply.github.com>
2024-11-28 22:29:11 +0800

    Create position.dart

---
+++ episode 23/position.dart
@@ -0,0 +1,8 @@
+// Positional: The position of an argument determines which parameter receives the value
+
+void add(a, b) { // a & b are positional parameters
+  print(a + b); // print() is a built-in function that will be explained later
+}
+ 
+add(5, 10); // 5 is used as a value for a, because it's the first argument; 10 is used as a value for b
+
