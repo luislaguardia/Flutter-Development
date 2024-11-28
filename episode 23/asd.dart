ee055156a8538e8be6d240aa1d5fe2f88d25f2d7
Luis Laguardia <61608109+luislaguardia@users.noreply.github.com>
2024-11-28 22:30:03 +0800

    Create asd.dart

---
+++ episode 23/asd.dart
@@ -0,0 +1,5 @@
+void add({a, b}) { // a & b are named parameters (because of the curly braces)
+  print(a + b); 
+}  
+ 
+add(b: 5, a: 10); // 5 is used as a value for b, because it's assigned to that name; 10 is used as a value for a
