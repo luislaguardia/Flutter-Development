a5ba2c9ff86dffa04bb43281a0c9dab840a725dc
Luis Laguardia <61608109+luislaguardia@users.noreply.github.com>
2024-12-01 22:27:59 +0800

    Update main.dart

--- Practice/Increment and Decrement/main.dart
+++ Practice/Increment and Decrement/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'My App ni Luis'),
+      home: const MyHomePage(title: 'Increment and Decrement'),
     );
   }
 }
