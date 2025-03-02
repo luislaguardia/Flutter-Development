2e98ebf9e77f0e529d0629aa345f8f539ddff78d
awit salvatore <61608109+luislaguardia@users.noreply.github.com>
2025-03-02 21:02:58 +0800

    Create dashboard_screen.dart

---
+++ Login/dashboard_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class DashboardScreen extends StatelessWidget {
+  const DashboardScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: const Text("Dashboard"),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () {
+              Navigator.pushReplacementNamed(context, '/login');
+            },
+          ),
+        ],
+      ),
+      body: const Center(
+        child: Text(
+          "Welcome to your Dashboard!",
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
